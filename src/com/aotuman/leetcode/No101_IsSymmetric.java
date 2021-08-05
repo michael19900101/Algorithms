2d8e603bc605d50eb2c2fc9a817c964c2ef29b9b
huangkunting <382968311@qq.com>
2021-08-05 17:36:51 +0800

    101.对称二叉树

---
+++ src/com/aotuman/leetcode/No101_IsSymmetric.java
@@ -0,0 +1,57 @@
+package com.aotuman.leetcode;
+
+/**
+ * 101.对称二叉树
+ *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * səˈmetrik(ə)l
+ */
+public class No101_IsSymmetric {
+
+    public static void main(String[] args) {
+        No101_IsSymmetric solution = new No101_IsSymmetric();
+        TreeNode root = solution.testData();
+        boolean res = solution.isSymmetric(root);
+        System.out.println(res);
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        return check(root, root);
+    }
+
+    // 递归方式
+    public boolean check(TreeNode leftTree, TreeNode rightTree){
+        if (leftTree == null && rightTree == null) return true;
+        if (leftTree == null || rightTree == null) return false;
+        return leftTree.val == rightTree.val
+                && check(leftTree.left, rightTree.right)
+                && check(leftTree.right, rightTree.left);
+    }
+
+    public TreeNode testData() {
+        TreeNode rootNode = new TreeNode(1);
+        rootNode.left = new TreeNode(2);
+        rootNode.right = new TreeNode(2);
+        rootNode.left.left = new TreeNode(3);
+        rootNode.left.right = new TreeNode(4);
+        rootNode.right.left = new TreeNode(4);
+        rootNode.right.right = new TreeNode(3);
+        return rootNode;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() { }
+        TreeNode(int val) {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
